f64ab33b8a15091565d33ce61d9cf8a7ad893973
AndrewBas <baskrut@gmail.com>
2020-08-22 01:44:31 +0300

    Parser works properly

--- app/src/main/java/com/andrewbas/basiptv/Utils/M3UParser.kt
+++ app/src/main/java/com/andrewbas/basiptv/Utils/M3UParser.kt
@@ -10,49 +10,30 @@ import com.andrewbas.basiptv.TvChanel
 работа с классом через parser.parse().save()*/
 
 class M3UParser(private val playList: List<String>) {
-
-//private val EXT = "#EXT"       нужно было чтоб не использовать regex
-//    private val HTTP = "HTTP"     нужно было чтоб не использовать regex
-
     fun parse(): TvChanel{
-
         var logo = ""
         var type = ""
         var chNumber = "0"
         var title = ""
         var sourse = ""
+        var splittedLine = listOf("")
 
+            for(currentLine in playList){
 
-var splittedLine = listOf(" ")
-        for(currentLine in playList){
+                if ("^#EXTINF".toRegex().containsMatchIn(currentLine)) {// без regex -> currentLine.startsWith(EXT)
+                    splittedLine = currentLine.split(",", " ", ignoreCase = true, limit = 0)
+                }
+                else if ("^http".toRegex().containsMatchIn(currentLine)){// -> без regex currentLine.startsWith(HTTP)
 
-            if ("^#EXTINF".toRegex().containsMatchIn(currentLine)) {// без regex -> currentLine.startsWith(EXT)
-                splittedLine = currentLine.split(",", " ", ignoreCase = true, limit = 0)
+                    title = getTitle(splittedLine)
+                    type = getType(splittedLine)
+                    chNumber = getChNumber(splittedLine)
+                    logo = getLogo(splittedLine)
+                    sourse = getSource(currentLine)  // строка начинающаяся с HTTP априори ссылка на видео
+                    getChNumber(splittedLine)
+                }
             }
-            else if ("^http".toRegex().containsMatchIn(currentLine)){// -> без regex currentLine.startsWith(HTTP)
-
-
-               title = getTitle(splittedLine)
-
-                type = getType(splittedLine)
-                chNumber = splittedLine[2]
-
-                logo = getLogo(splittedLine)
-
-                sourse = getSource(currentLine)// строка начинающаяся с HTTP априори ссылка на видео
-
-//              getLogo(splittedLine)
-//                getType(splittedLine)
-//                getChNumber(splittedLine)
-            }
-        }
-       /* playList.forEach{
-            it.
-        }*/
-
-//return logo
-       return TvChanel(title, sourse, logo, type, chNumber, false)
-
+        return TvChanel(title, sourse, logo, type, chNumber, false)
     }
 
     fun getTitle(splitedLine: List<String>): String{//во ???всех??? вариантах плейлистов название стоит последним
@@ -65,52 +46,43 @@ var splittedLine = listOf(" ")
 
     fun getLogo(splitedLine: List<String>): String{
         var logo = ""
-
         if (splitedLine.size > 2){ //больше двух потому что 1-> EXTINF , 2 -> название
-
             val pattern = "\\.(png|jpg|gif|svg|jpeg|swf)".toRegex()
             for(word in splitedLine){
                 if(pattern.containsMatchIn(word)){
-
-                    logo = word.replaceBefore("http", "")
-                    logo = logo.dropLast(1)/*replace(logo[logo.length-1], "")*/
+                    logo = word.replaceBefore("http", "").dropLast(1)
                 }
             }
         }
         return logo
     }
-   fun getType(splitedLine: List<String>): String{//todo доделать чтоб правильно обрезало
 
+    fun getType(splitedLine: List<String>): String{
         var type = ""
-
-        if (splitedLine.size > 2){//больше двух потому что 1-> EXTINF , 2 -> название
-
-            val pattern = "(group-title)".toRegex()
+        if (splitedLine.size > 2){                          //больше двух потому что 1-> EXTINF , 2 -> название
+            val pattern = "group-title=".toRegex()
             for(word in splitedLine){
                 if(pattern.containsMatchIn(word)){
-                    type = word.replaceBefore('=', "")
-                }
+                    type = word.replace(pattern, "").trim('\"') }
             }
         }
         return type
     }
 
-       fun getChNumber(splitedLine: List<String>): String{//todo доделать чтоб правильно обрезало
+    fun getChNumber(splitedLine: List<String>): String{
+        var chNumber = ""
+        if (splitedLine.size > 2){                        //больше двух потому что 1-> EXTINF , 2 -> название
+            val pattern = "tvg-name=".toRegex()
+            for(word in splitedLine){
+                if(pattern.containsMatchIn(word)){
+                    chNumber = word.replace(pattern, "").trim('\"')
+                }
+            }
+        }
+        return chNumber
+    }
 
-          var chNumber = ""
-          if (splitedLine.size > 2){//больше двух потому что 1-> EXTINF , 2 -> название
-
-              val pattern = "(tvg-name)".toRegex()
-              for(word in splitedLine){
-                  if(pattern.containsMatchIn(word)){
-                      chNumber = word.replaceBefore('=', "")
-                  }
-              }
-          }
-          return chNumber
-      }
-
-    fun getSource(currentLine: String): String { // строка начинающаяся с HTTP априори ссылка на видео
+    fun getSource(currentLine: String): String {            // строка начинающаяся с HTTP априори ссылка на видео
         return currentLine
     }
 }
--- app/src/test/java/com/andrewbas/basiptv/UnitTest.kt
+++ app/src/test/java/com/andrewbas/basiptv/UnitTest.kt
@@ -16,18 +16,12 @@ class UnitTest {
     }
 
     @Test
-    fun parserGetLogo_isCorrect(){
+    fun parser_parse_isCorrect(){
 
-//        var m3uParser = M3UParser
         val testList = listOf<String>("#EXTINF:0 group-title=\"Украина\" tvg-name=\"3\" tvg-logo=\"http://ott.odessa.tv/images/logos/104.png\",1+1",
             "http://10.0.0.59:8084/live_mask/104/video1.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC81LzIwMjAgMTA6MjA6NTggUE0maGFzaF92YWx1ZT1wbzZJVW9PVkhaaUV3b0M0V04vSUFnPT0mdmFsaWRtaW51dGVzPTE0NDAmaWQ9NDAtcGMmc3RybV9sZW49MTM=&user=40&access=full"
             )
 
-        /*val testSplitedLine = listOf("#EXTINF:0",
-            "group-title=\"Украина\"",
-            "tvg-name=\"3\"",
-            "tvg-logo=\"http://ott.odessa.tv/images/logos/104.png\"",
-            "1+1")*/
 
         val expected = TvChanel("1+1",
             "http://10.0.0.59:8084/live_mask/104/video1.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC81LzIwMjAgMTA6MjA6NTggUE0maGFzaF92YWx1ZT1wbzZJVW9PVkhaaUV3b0M0V04vSUFnPT0mdmFsaWRtaW51dGVzPTE0NDAmaWQ9NDAtcGMmc3RybV9sZW49MTM=&user=40&access=full",
