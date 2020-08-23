2182fc4bdc71ef365a634e14d727353aeec1adac
AndrewBas <baskrut@gmail.com>
2020-08-24 00:48:20 +0300

    Add Activity for loading pl

---
+++ app/src/main/java/com/andrewbas/basiptv/AddPlayListActivity.kt
@@ -0,0 +1,12 @@
+package com.andrewbas.basiptv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddPlayListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_play_list)
+    }
+}
--- app/src/main/java/com/andrewbas/basiptv/MainActivity.kt
+++ app/src/main/java/com/andrewbas/basiptv/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.andrewbas.basiptv
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,5 +9,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*if()
+
+   goToAddPlaylistActivity()
+    */
     }
+
+   fun  goToAddPlaylistActivity(){
+       intent = Intent(this, AddPlayListActivity::class.java)
+       startActivity(intent)
+   }
+
 }
